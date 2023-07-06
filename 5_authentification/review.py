d9c1d35c541710b9a2fb4feab782e281c8901682
David Doan <daviddoan@Davids-MacBook-Pro.local>
2023-07-06 11:00:10 -0600

    Solution 5

--- 5_authentification/review.py
+++ 5_authentification/review.py
@@ -14,3 +14,24 @@
 #     rewrites,
 #   };
 # };
+
+# create vite@latest client
+# import { defineConfig } from 'vite'
+# import react from '@vitejs/plugin-react'
+
+# // https://vitejs.dev/config/
+# export default defineConfig({
+#   plugins: [react()],
+#   server: {
+#     port: 3000,
+#     cors:true,
+#     proxy: {
+#       "/api":{
+#         target: "http://127.0.0.1:5555",
+#         changeOrigin:true,
+#         secure: false,
+#         rewrite: (path)=>path.replace(/^\/api/,"")
+#       }
+#     }
+#   }
+# })
