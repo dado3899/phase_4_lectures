0ecb03bca8a9430b8cf6d8b72d8489d201a92085
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-01-08 09:56:14 -0700

    Wording changes

--- 1_Flask_Introduction/server/models.py
+++ 1_Flask_Introduction/server/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 # This will be our base, we can use our sqlalchemy method of making models using this!
 db = SQLAlchemy()
 
-# Lets create a class, first lets Pseudocode out the class
+# Lets create a class
 class NewClass(db.Model):
     __tablename__ = ''
     id = db.Column(db.Integer, primary_key=True)
