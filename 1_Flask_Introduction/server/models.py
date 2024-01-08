b25eb529e3eb181c90b5a45a59b513ce957dc942
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-01-08 10:09:16 -0700

    Added links for metadata reading

--- 1_Flask_Introduction/server/models.py
+++ 1_Flask_Introduction/server/models.py
@@ -1,8 +1,17 @@
-# Import SQLAlchemy from flask
+# Import SQLAlchemy from flask and from sqlalchemy
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
+# This piece of code here i
+metadata = MetaData(naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+      })
 # This will be our base, we can use our sqlalchemy method of making models using this!
-db = SQLAlchemy()
+db = SQLAlchemy(metadata=metadata)
 
 # Lets create a class
 class NewClass(db.Model):
